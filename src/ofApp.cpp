4d5ba00ea6f5db6edb2863f9f6edd89577a780e0
will <willmercedes@gmail.com>
2016-07-03 01:36:50 +0800

    add
    7

---
+++ src/ofApp.cpp
@@ -0,0 +1,8 @@
+#include "ofApp.h"
+
+void ofApp::setup(){
+    
+    
+}
+
+
---
+++ src/ofApp.h
@@ -0,0 +1,10 @@
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+};
